d78e7f8cb30f779eafb23790f656bbe757af608f
Виталий Сопов <vitaliy-svs@reg.ru>
2026-02-04 23:19:41 +0300

    Рефакторим MainPage

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,8 +9,6 @@ import (
 // Хранилище сокращений, ключ -- хеш сокращения, значение -- сокращаемый URL
 type Shorted map[string]string
 
-var shorts Shorted
-
 // Генератор хеша сокращения
 func cryptoRandomString(length int) (string, error) {
 	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -28,83 +26,85 @@ func cryptoRandomString(length int) (string, error) {
 }
 
 // Хендлер основной страницы
-func mainPage(res http.ResponseWriter, req *http.Request) {
-	// Проверяем методы запросов, должны быть только POST и GET
-	if req.Method != http.MethodGet && req.Method != http.MethodPost {
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// POST запрос должен быть с Content-Type `text/plain`
-	if req.Method == http.MethodPost && req.Header.Get("Content-Type") != "text/plain" {
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Проверяем URL для POST запроса
-	if req.Method == http.MethodPost && req.URL.Path != "/" {
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Если POST запрос, читаем параметры запроса, в параметрах должен быть URL для сокращения
-	if req.Method == http.MethodPost {
-		defer req.Body.Close()
-		body, err := io.ReadAll(req.Body)
-		if err != nil {
+func MainPage(shorts Shorted) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		// Проверяем методы запросов, должны быть только POST и GET
+		if req.Method != http.MethodGet && req.Method != http.MethodPost {
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		if string(body) == "" {
+		// POST запрос должен быть с Content-Type `text/plain`
+		if req.Method == http.MethodPost && req.Header.Get("Content-Type") != "text/plain" {
 			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		targetValue := string(body)
-
-		// Ищем body запроса в значениях уже сокращённых
-		var cryptoString string
-		for key, value := range shorts {
-			if value == targetValue {
-				cryptoString = key
-			}
+		// Проверяем URL для POST запроса
+		if req.Method == http.MethodPost && req.URL.Path != "/" {
+			res.WriteHeader(http.StatusBadRequest)
+			return
 		}
 
-		if cryptoString == "" {
-			// Если не нашли, генерируем новое сокращение и записываем его в shorts
-			str, err := cryptoRandomString(8)
+		// Если POST запрос, читаем параметры запроса, в параметрах должен быть URL для сокращения
+		if req.Method == http.MethodPost {
+			defer req.Body.Close()
+			body, err := io.ReadAll(req.Body)
 			if err != nil {
-				panic(err)
+				res.WriteHeader(http.StatusBadRequest)
+				return
 			}
 
-			cryptoString = "/" + str
+			if string(body) == "" {
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
 
-			shorts[cryptoString] = targetValue
+			targetValue := string(body)
+
+			// Ищем body запроса в значениях уже сокращённых
+			var cryptoString string
+			for key, value := range shorts {
+				if value == targetValue {
+					cryptoString = key
+				}
+			}
+
+			if cryptoString == "" {
+				// Если не нашли, генерируем новое сокращение и записываем его в shorts
+				str, err := cryptoRandomString(8)
+				if err != nil {
+					panic(err)
+				}
+
+				cryptoString = "/" + str
+
+				shorts[cryptoString] = targetValue
+			}
+
+			res.Header().Set("content-type", "text/plain")
+			res.WriteHeader(http.StatusCreated)
+			res.Write([]byte("http://" + req.Host + cryptoString))
+			return
 		}
 
-		res.Header().Set("content-type", "text/plain")
-		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte("http://" + req.Host + cryptoString))
-		return
-	}
+		// GET запрос, пытаемся найти сокращение в shorts по ключу
+		if shorts[req.URL.Path] == "" {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-	// GET запрос, пытаемся найти сокращение в shorts по ключу
-	if shorts[req.URL.Path] == "" {
-		res.WriteHeader(http.StatusBadRequest)
-		return
+		res.Header().Set("Location", shorts[req.URL.Path])
+		res.WriteHeader(http.StatusTemporaryRedirect)
+		res.Write([]byte(shorts[req.URL.Path]))
 	}
-
-	res.Header().Set("Location", shorts[req.URL.Path])
-	res.WriteHeader(http.StatusTemporaryRedirect)
-	res.Write([]byte(shorts[req.URL.Path]))
 }
 
 func main() {
-	shorts = make(Shorted)
+	shorts := make(Shorted)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc(`/`, MainPage(shorts))
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
