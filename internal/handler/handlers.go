b20d752588b52eb5605af0a3cf62746e87c08b1b
Виталий Сопов <vitaliy-svs@reg.ru>
2026-03-20 23:31:23 +0300

    Убираем очистку данных 2

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -13,7 +13,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"os"
+	// "os"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -260,29 +260,29 @@ func UserURLS(storage *model.Storage, cfg *config.Config) http.HandlerFunc {
 		cookie, err := req.Cookie("jwt")
 		if err != nil {
 			if err == http.ErrNoCookie {
-				// Очистим "чужие" сокращения из памяти
-				clear(storage.Shorted)
-				clear(storage.Full)
+				// // Очистим "чужие" сокращения из памяти
+				// clear(storage.Shorted)
+				// clear(storage.Full)
 
-				// Если используется база, вычистим из неё "чужие" сокращения
-				if cfg.DBconnStr != "" {
-					err = repository.ClearDB(cfg)
-					if err != nil {
-						log.Println(err)
-						res.WriteHeader(http.StatusInternalServerError)
-						return
-					}
-				}
+				// // Если используется база, вычистим из неё "чужие" сокращения
+				// if cfg.DBconnStr != "" {
+				// 	err = repository.ClearDB(cfg)
+				// 	if err != nil {
+				// 		log.Println(err)
+				// 		res.WriteHeader(http.StatusInternalServerError)
+				// 		return
+				// 	}
+				// }
 
-				// Если используется файл, вычистим из него "чужие" сокращения
-				if cfg.StorageFileName != "" {
-					err = os.Truncate(cfg.StorageFileName, 0)
-					if err != nil {
-						log.Println(err)
-						res.WriteHeader(http.StatusInternalServerError)
-						return
-					}
-				}
+				// // Если используется файл, вычистим из него "чужие" сокращения
+				// if cfg.StorageFileName != "" {
+				// 	err = os.Truncate(cfg.StorageFileName, 0)
+				// 	if err != nil {
+				// 		log.Println(err)
+				// 		res.WriteHeader(http.StatusInternalServerError)
+				// 		return
+				// 	}
+				// }
 
 				tokenString, err = userauth.BuildJWTString()
 				if err != nil {
